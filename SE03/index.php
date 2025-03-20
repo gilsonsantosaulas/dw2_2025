1a6e5656c5d88b5ed8234fcedb3c944d3eb0879e
gilsonsatu <gilsonsatu@gmail.com>
2025-03-20 10:36:45 -0400

    SE03

---
+++ SE03/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+  <form action="calcula.php" method="GET"> 
+    <label for="">Número 1</label>
+    <input type="text" name="numero1">
+    <label for="">Número 2</label>
+    <input type="text" name="numero2">
+    <button type="submit">Calcular</button>
+    </form>
+  </body>
+</html>
